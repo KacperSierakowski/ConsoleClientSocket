c39180774e4890ca3f53a22ecbdd0e2108a534d7
Student218608 <218608@student.pwr.edu.pl>
2019-04-29 15:21:32 +0200

    First

--- ConsoleClientSocket/Program.cs
+++ ConsoleClientSocket/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +12,36 @@ namespace ConsoleClientSocket
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Client");
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse("192.168.1.103"), 8888);
+            socket.Connect(iPEnd);
+
+            string message;
+            do
+            {
+                Console.WriteLine("Write a message:");
+                message = Console.ReadLine();
+                try
+                {
+                    socket.Send(System.Text.Encoding.UTF8.GetBytes(message));
+
+                    byte[] piggybackData = new byte[2];
+                    socket.Receive(piggybackData);
+                    Console.WriteLine("Piggyback data :" + System.Text.Encoding.UTF8.GetString(piggybackData));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Connection aborted. " + e.ToString());
+                }
+            } while (message.Length > 0);
+
+
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
         }
     }
 }
